b165fe983e2755dae98ba2d2e2bfba72b9ef1e7d
Orson Saint Fleur <orsonsaintfleurgmail.com>
2020-07-06 22:09:40 -0400

    get operator function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+let num = document.getElementById('initialNum')
 
+function calc(){
+
+  function minus( ){
+
+  }
+  function plus(){
+
+  }
+  function multiply(){
+
+  }
+  function divide(){
+
+  }
+}
+fdm consultant
